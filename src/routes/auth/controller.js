30748d4193658ed1d4e62c9d34a3a16f81adfed2
Jodu555 <Jodu505@gmail.com>
2021-10-09 19:17:46 +0200

    Impemented the auth route

---
+++ src/routes/auth/controller.js
@@ -0,0 +1,78 @@
+const { userRegisterSchema, userLoginSchema } = require('../../database/schemas');
+const { v4 } = require('uuid');
+const { Database } = require('@jodu555/mysqlapi');
+const database = Database.getDatabase();
+const authManager = require('../../utils/authManager');
+const bcrypt = require('bcryptjs');
+
+const register = async (req, res, next) => {
+    const validation = userRegisterSchema.validate(req.body);
+    if (validation.error) {
+        res.json(jsonError(validation.error.details[0].message));
+    } else {
+        const user = validation.value
+        const search = { ...user }; //Spreading to disable the reference
+        delete search.password;
+        search.unique = true;
+        const result = await database.get('accounts').get(search);
+
+        if (result.length == 0) {
+            const obj = jsonSuccess('Registered');
+            user.uuid = v4();
+            user.password = await bcrypt.hash(user.password, 8);
+            await database.get('accounts').create(user);
+
+            delete user.password;
+            obj.user = user;
+            res.json(obj);
+        } else {
+            res.json(jsonError('The email or the username is already taken!'));
+        }
+    }
+};
+
+const login = async (req, res, next) => {
+    const validation = userLoginSchema.validate(req.body);
+    if (validation.error) {
+        res.json(jsonError(validation.error.details[0].message));
+    } else {
+        const user = validation.value;
+        const result = await database.get('accounts').get({ username: user.username, unique: true });
+        if (result.length > 0) {
+            if (await bcrypt.compare(user.password, result[0].password)) {
+                const obj = jsonSuccess('Successfully logged In');
+                const token = v4();
+                obj.token = token;
+                authManager.addToken(token, result[0]);
+                res.json(obj);
+            } else {
+                res.json(jsonError('Invalid password!'));
+            }
+        } else {
+            const value = user.username ? 'username' : 'email';
+            res.json(jsonError('Invalid ' + value + '!'));
+        }
+    }
+};
+
+const logout = async (req, res, next) => {
+    const token = req.credentials.token;
+    authManager.removeToken(token);
+    res.json(jsonSuccess('Successfully logged out!'))
+};
+
+// function generateVerificationToken(len) {
+//     let token = '';
+//     for (let i = 0; i < len; i++) {
+//         token += v4();
+//     }
+//     token = token.split('-').join('');
+//     return token;
+// };
+
+module.exports = {
+    register,
+    login,
+    logout,
+    emailValidation
+}
---
+++ src/routes/auth/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const controller = require('./controller');
+const { jsonSuccess } = require('../../utils/jsonMessages');
+const authManager = require('../../utils/authManager');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json(jsonSuccess('Auth-Router works just fine'));
+});
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.get('/logout', authManager.authentication, controller.logout);
+
+module.exports = {
+    router,
+};
