035e3328206328e760954b1ad8a2fff1b3fa44c9
Jodu555 <Jodu505@gmail.com>
2021-10-10 11:15:33 +0200

    removed the password validation from 8 chars to 5 chars

--- src/database/schemas.js
+++ src/database/schemas.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const userRegisterSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().alphanum().min(8).max(50).required(),
+    password: Joi.string().alphanum().min(5).max(50).required(),
     email: Joi.string().email().required(),
 });
 
