6af359fac460089f42a720764c4f6686e160ca53
Jodu555 <Jodu505@gmail.com>
2021-10-10 10:53:11 +0200

    Fixed an dont exist module

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const controller = require('./controller');
-const { jsonSuccess } = require('../../utils/jsonMessages');
 const authManager = require('../../utils/authManager');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json(jsonSuccess('Auth-Router works just fine'));
+    res.json({ message: 'Auth-Router works just fine' });
 });
 router.post('/register', controller.register);
 router.post('/login', controller.login);
