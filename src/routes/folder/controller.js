63965a6aa3c91061e0bbd55bcaa3e551e4598a3d
Jodu555 <Jodu505@gmail.com>
2021-10-12 11:49:29 +0200

    Added folder controller and importet all necessary libs

---
+++ src/routes/folder/controller.js
@@ -0,0 +1,4 @@
+const { folderCreationSchema } = require('../../database/schemas');
+const { v4 } = require('uuid');
+const { Database } = require('@jodu555/mysqlapi');
+const database = Database.getDatabase();
