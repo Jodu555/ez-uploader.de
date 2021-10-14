62330edec8c818de2f22ec1e915cffecf9ec0fc5
Jodu555 <Jodu505@gmail.com>
2021-10-14 12:11:43 +0200

    made some fixes and also the parentuuid change prevention for the root folder

--- src/routes/folder/controller.js
+++ src/routes/folder/controller.js
@@ -64,7 +64,7 @@ const update = async (req, res, next) => {
             return;
         }
         //Check if the user want to edit the name of the root folder
-        if (folder.name && database.get('folders').getRootFolder(account_UUID).UUID == uuid) {
+        if ((folder.name || folder.parent_UUID) && (await database.get('folders').actions.getRootFolder(account_UUID)).UUID == uuid) {
             next(new Error('You cannot change the name of the root folder!'));
             return;
         }
