783e2f3c0c882e18e5a37de462a8335854743f47
Jodu555 <Jodu505@gmail.com>
2021-10-16 13:54:05 +0200

    Finalized the get entry function

--- src/routes/entry/controller.js
+++ src/routes/entry/controller.js
@@ -3,10 +3,17 @@ const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 
 const get = async (req, res, next) => {
-    const account_UUID = req.credentials.user.UUID;
-
+    try {
+        const account_UUID = req.credentials.user.UUID;
+        const result = await database.get('entrys').actions.getAllFromAccount(account_UUID);
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
 }
 
+
 module.exports = {
-    get
+    get,
+    getFromFolder
 }
--- src/routes/entry/index.js
+++ src/routes/entry/index.js
@@ -4,6 +4,7 @@ const authManager = require('../../utils/authManager');
 const router = express.Router();
 
 router.get('/', controller.get); // Returns all the entrys a user owns
+router.get('/:folderUUID', controller.getFromFolder); // Returns all the entrys a user owns from a specific folder
 
 module.exports = {
     router,
