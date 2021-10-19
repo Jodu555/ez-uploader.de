0c7b931546448edd8f8688c1653f64cc652c0aee
Jodu555 <Jodu505@gmail.com>
2021-10-19 20:42:32 +0200

    Setted up the router for upload

---
+++ src/routes/uploader/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const authManager = require('../../utils/authManager');
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+
+});
+
+module.exports = {
+    router,
+};
