f8fcf11c7d350bbefb49ded30681047221716904
Jodu555 <Jodu505@gmail.com>
2021-10-20 14:16:41 +0200

    added the complete file renametion + added a basic file validation

--- src/routes/uploader/index.js
+++ src/routes/uploader/index.js
@@ -5,21 +5,46 @@ const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'upload/');
+    },
+    filename: (req, file, cb) => {
+        console.log(1, req.api);
+        console.log(2, file);
+        const fileName = req.api + '.' + file.originalname.toLowerCase().split('.')[1];
+        cb(null, fileName)
+    }
+});
 
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg formats allowed!'));
+        }
+    }
+}).single('image');
 
 router.post('/', async (req, res, next) => {
     const account_UUID = req.credentials.user.UUID;
+    const entry = {
+        UUID: v4(),
+        folder_UUID: (await database.get('folders').actions.getRootFolder(account_UUID)).UUID,
+        public: 0,
+        share: 0,
+    }
+    req.api = entry.UUID;
+
     await upload(req, res, async (err) => {
         if (err) {
             next(err);
             return;
         }
-        const entry = {
-            UUID: v4(),
-            folder_UUID: (await database.get('folders').actions.getRootFolder(account_UUID)).UUID,
-            public: 0,
-            share: 0,
-        }
+
         console.log(entry);
         res.json(entry);
         // const created = database.get('entrys').create(entry);
