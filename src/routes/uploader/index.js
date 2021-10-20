d56bea2b66f3a1a922b6c6119260f38181a77b40
Jodu555 <Jodu505@gmail.com>
2021-10-20 11:48:51 +0200

    Implemented the multer stuff

--- src/routes/uploader/index.js
+++ src/routes/uploader/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const multer = require('multer');
 const authManager = require('../../utils/authManager');
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
 
+const upload = multer().single('image');
+
+router.post('/', (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err)
+            throw err;
+        // if (err instanceof multer.MulterError) {
+        // } else if (err) {
+        // }
+
+    })
 });
 
 module.exports = {
