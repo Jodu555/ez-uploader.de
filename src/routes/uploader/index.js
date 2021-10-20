892507f1acbf63bd3f56d7700549732bf41c91e6
Jodu555 <Jodu505@gmail.com>
2021-10-20 14:06:23 +0200

    added the entry generation

--- src/routes/uploader/index.js
+++ src/routes/uploader/index.js
@@ -5,15 +5,25 @@ const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 
-const upload = multer({ dest: 'upload/' }).single('image');
+
 
 router.post('/', async (req, res, next) => {
+    const account_UUID = req.credentials.user.UUID;
     await upload(req, res, async (err) => {
         if (err) {
             next(err);
             return;
         }
-
+        const entry = {
+            UUID: v4(),
+            folder_UUID: (await database.get('folders').actions.getRootFolder(account_UUID)).UUID,
+            public: 0,
+            share: 0,
+        }
+        console.log(entry);
+        res.json(entry);
+        // const created = database.get('entrys').create(entry);
+        // res.json(created);
     })
 });
 
