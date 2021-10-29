98a838b22f13e418535153210a58a455d6edb427
Jodu555 <Jodu505@gmail.com>
2021-10-29 17:07:39 +0200

    added the image serving with auth restriction

--- src/routes/image/index.js
+++ src/routes/image/index.js
@@ -3,8 +3,20 @@ const { v4 } = require('uuid');
 const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
+const authManager = require('../../utils/authManager');
+const path = require('path');
 
-router.get('/:uuid', (req, res, next) => {
+router.get('/:UUID', (req, res, next) => {
+    const UUID = req.params.UUID
+    //Check if user has this key
+    console.log(UUID);
+    console.log(req.query.key);
+    if (authManager.getUser(req.query.key)) {
+        const imagePath = path.join(__dirname, '../../../upload', UUID + '.png');
+        console.log(imagePath);
+        res.sendFile(imagePath);
+    }
+
     console.log('Image Request');
 })
 
