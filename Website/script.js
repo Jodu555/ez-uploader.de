14232fca1227ff500b89f8eb5f82e5dd04a7f8af
Jodu555 <Jodu505@gmail.com>
2021-10-30 20:01:55 +0200

    added a little feature to see the latest change for the project based on my new api

---
+++ Website/script.js
@@ -0,0 +1,34 @@
+const API_URL = 'http://github-stats.jodu555.de/';
+
+let lastChange;
+
+(async () => {
+    loadLastChange();
+})();
+
+
+async function loadLastChange() {
+    const response = await fetch(API_URL + 'api/lastCommit/Jodu555/ez-uploader.de');
+    const json = await response.json();
+    lastChange = json.data.info.lastUpdated;
+}
+
+setInterval(() => {
+    animateCountDown('last-change-', lastChange);
+}, 1000);
+
+function animateCountDown(prefix, till) {
+    const now = Date.now();
+    const diff = now - till
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    document.querySelector('#' + prefix + 'day').innerText = Math.floor(diff / day);
+    document.querySelector('#' + prefix + 'hour').innerText = Math.floor((diff % day) / hour);
+    document.querySelector('#' + prefix + 'minute').innerText = Math.floor((diff % hour) / minute);
+    document.querySelector('#' + prefix + 'second').innerText = Math.floor((diff % minute) / second);
+
+}
