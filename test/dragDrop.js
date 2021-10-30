964c5f0da9120d4bfdda8c62a212f746ad003017
Jodu555 <Jodu505@gmail.com>
2021-10-30 14:20:45 +0200

    added the init function to re edit the elements on load

--- test/dragDrop.js
+++ test/dragDrop.js
@@ -1,40 +1,46 @@
-const draggables = document.querySelectorAll('.draggable');
-const places = document.querySelectorAll('.place');
+let draggables = document.querySelectorAll('.draggable');
+let places = document.querySelectorAll('.place')
 
 const highlightClass = 'table-info';
 const errorClass = 'table-danger';
 
-draggables.forEach((draggable) => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging');
-    });
-    draggable.addEventListener('dragend', (e) => {
-        draggable.classList.remove('dragging');
-        const place = getPlaceOver(e);
-        if (place) {
-            place.classList.remove(highlightClass)
-            place.classList.remove(errorClass)
-            if (place != draggable) {
-                console.log('droppedInto', place);
-                draggable.remove();
+initDragDrop();
+
+function initDragDrop() {
+    console.log(123);
+    draggables = document.querySelectorAll('.draggable');
+    places = document.querySelectorAll('.place');
+    draggables.forEach((draggable) => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add('dragging');
+        });
+        draggable.addEventListener('dragend', (e) => {
+            draggable.classList.remove('dragging');
+            const place = getPlaceOver(e);
+            if (place) {
+                place.classList.remove(highlightClass)
+                place.classList.remove(errorClass)
+                if (place != draggable) {
+                    draggable.remove();
+                }
             }
-        }
+        });
     });
-});
 
-places.forEach((place) => {
-    place.addEventListener('dragleave', (e) => {
-        e.preventDefault();
-        place.classList.remove(highlightClass);
-        place.classList.remove(errorClass);
-    })
-    place.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        console.log(place.classList.contains('dragging'));
-        place.classList.contains('dragging') ? place.classList.add(errorClass) : place.classList.add(highlightClass);
-
+    places.forEach((place) => {
+        place.addEventListener('dragleave', (e) => {
+            e.preventDefault();
+            place.classList.remove(highlightClass);
+            place.classList.remove(errorClass);
+        })
+        place.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            place.classList.contains('dragging') ? place.classList.add(errorClass) : place.classList.add(highlightClass);
+        });
     });
-});
+}
+
+
 
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
