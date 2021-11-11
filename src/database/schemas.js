93b8fa1c63b5d72a0f807d8245ad4e9d3078e237
Jodu555 <Jodu505@gmail.com>
2021-11-11 12:33:37 +0100

    setted the pw length down to 3 for now

--- src/database/schemas.js
+++ src/database/schemas.js
@@ -2,14 +2,14 @@ const Joi = require('joi');
 
 const userRegisterSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().alphanum().min(5).max(50).required(),
+    password: Joi.string().alphanum().min(3).max(50).required(),
     email: Joi.string().email().required(),
 });
 
 const userLoginSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(30),
     email: Joi.string().email(),
-    password: Joi.string().alphanum().min(5).max(50).required(),
+    password: Joi.string().alphanum().min(3).max(50).required(),
 }).xor('username', 'email');
 
 const folderCreationSchema = Joi.object({
