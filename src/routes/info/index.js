c457e75a6cc33a011b18a92773d94e8ce390c71d
Jodu555 <Jodu505@gmail.com>
2021-11-22 17:24:11 +0100

    wrote out the cache and info route

--- src/routes/info/index.js
+++ src/routes/info/index.js
@@ -3,8 +3,26 @@ const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 
-router.get('/', (req, res, next) => {
+const cacheTime = 1000 * 60;
 
+router.get('/', async (req, res, next) => {
+    try {
+        if (database.cache.infos && (database.cache.infos.time <= Date.now())) {
+            res.json(database.cache.infos);
+        } else {
+            database.cache = database.cache || {};
+            const accounts = await database.get('accounts').get();
+            const entrys = await database.get('entrys').get();
+            database.cache.infos = {
+                time: Date.now() + cacheTime,
+                accounts: accounts.length,
+                entrys: entrys.length,
+            }
+            res.json(database.cache.infos);
+        }
+    } catch (error) {
+        next(error);
+    }
 })
 
 
