cdb82a7595a60f55f3aad78bd652ea450938c910
Jodu555 <Jodu505@gmail.com>
2021-12-10 19:45:46 +0100

    updated the info cache to the og database implemented cache

--- src/routes/info/index.js
+++ src/routes/info/index.js
@@ -3,25 +3,41 @@ const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 
-const cacheTime = (1000 * 60 * 60 /** 1 Hour */) + 2; // 2 Hours
+const cacheTime = (1000 * 60 * 60 /** 1 Hour */) + 2;
+
+database.registerCache('infos', {
+    time: (1000 * 60 * 60 /** 1 Hour */) + 2, // 2 Hours
+    calls: 50
+}, async (param) => {
+    const accounts = await database.get('accounts').get();
+    const entrys = await database.get('entrys').get();
+    const folders = await database.get('folders').get();
+    return {
+        accounts: accounts.length,
+        entrys: entrys.length,
+        folders: folders.length
+    }
+})
 
 router.get('/', async (req, res, next) => {
     try {
-        if (database.cache && database.cache.infos && (database.cache.infos.time >= Date.now())) {
-            res.json({ ...database.cache.infos, cached: true });
-        } else {
-            database.cache = database.cache || {};
-            const accounts = await database.get('accounts').get();
-            const entrys = await database.get('entrys').get();
-            const folders = await database.get('folders').get();
-            database.cache.infos = {
-                time: Date.now() + cacheTime,
-                accounts: accounts.length,
-                entrys: entrys.length,
-                folders: folders.length
-            }
-            res.json({ ...database.cache.infos, cached: false });
-        }
+        const output = await database.getCache('infos').get();
+        res.json(output);
+        // if (database.cache && database.cache.infos && (database.cache.infos.time >= Date.now())) {
+        //     res.json({ ...database.cache.infos, cached: true });
+        // } else {
+        //     database.cache = database.cache || {};
+        //     const accounts = await database.get('accounts').get();
+        //     const entrys = await database.get('entrys').get();
+        //     const folders = await database.get('folders').get();
+        //     database.cache.infos = {
+        //         time: Date.now() + cacheTime,
+        //         accounts: accounts.length,
+        //         entrys: entrys.length,
+        //         folders: folders.length
+        //     }
+        //     res.json({ ...database.cache.infos, cached: false });
+        // }
     } catch (error) {
         next(error);
     }
