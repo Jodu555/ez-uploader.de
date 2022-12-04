7c545052503747bfe9641ad9088b34c3cb62fb84
Jodu555 <Jodu505@gmail.com>
2022-12-04 16:02:05 +0100

    Fixed the sourcecode button on the website + added the png jpg and svg decision with extensivle array list + linted some files

--- src/routes/image/index.js
+++ src/routes/image/index.js
@@ -1,26 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { v4 } = require('uuid');
 const router = express.Router();
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.getDatabase();
 const authManager = require('../../utils/authManager');
-const path = require('path');
 
 router.get('/:UUID', (req, res, next) => {
-    try {
-        const UUID = req.params.UUID
-        //TODO: here comes the point on share public etc.
-        //TODO: Here comes also the extension choose hin (png, jpg)
-        if (authManager.getUser(req.query.key)) {
-            const imagePath = path.join(__dirname, '../../../upload', UUID + '.png');
-            res.sendFile(imagePath);
-        } else {
-            next(new Error('Unauthorized!'));
-        }
-    } catch (error) {
-        next(error);
-    }
-})
-
+	try {
+		const UUID = req.params.UUID;
+		//TODO: here comes the point on share public etc.
+		//TODO: Here comes also the extension choose hin (png, jpg)
+		if (authManager.getUser(req.query.key)) {
+			const imagePath = path.join(__dirname, '../../../upload', UUID);
+			const possibleEndings = ['.png', '.jpg', '.svg'];
+			let final = '';
+			for (const ending of possibleEndings) {
+				if (fs.existsSync(imagePath + ending)) {
+					final = imagePath + ending;
+					break;
+				}
+			}
+			res.sendFile(final);
+		} else {
+			next(new Error('Unauthorized!'));
+		}
+	} catch (error) {
+		next(error);
+	}
+});
 
 module.exports = { router };
--- static/utils.js
+++ static/utils.js
@@ -1,68 +1,66 @@
-const API_URL = window.location.hostname == 'localhost' ? 'http://localhost:3100/' : 'https://dev.ez-uploader.de/';
+const API_URL = window.location.hostname == 'localhost' ? 'http://localhost:7474/' : 'https://dev.ez-uploader.de/';
 
 function alert(selector, success, message, cb, interval) {
-    const alert = document.querySelector(selector);
-    alert.classList.remove('alert-success');
-    alert.classList.remove('alert-danger');
-    alert.classList.add(success ? 'alert-success' : 'alert-danger');
-    alert.innerText = message;
-    alert.style.display = '';
-    setTimeout(() => {
-        alert.style.display = 'none';
-        if (cb)
-            cb();
-    }, interval || 5000);
+	const alert = document.querySelector(selector);
+	alert.classList.remove('alert-success');
+	alert.classList.remove('alert-danger');
+	alert.classList.add(success ? 'alert-success' : 'alert-danger');
+	alert.innerText = message;
+	alert.style.display = '';
+	setTimeout(() => {
+		alert.style.display = 'none';
+		if (cb) cb();
+	}, interval || 5000);
 }
 
 function delSwal(title, cb) {
-    Swal.fire({
-        title,
-        text: 'This deletion cannot be undone!',
-        showDenyButton: true,
-        confirmButtonText: "Yes I'am",
-        denyButtonText: `No I dont`,
-    }).then(cb);
+	Swal.fire({
+		title,
+		text: 'This deletion cannot be undone!',
+		showDenyButton: true,
+		confirmButtonText: "Yes I'am",
+		denyButtonText: `No I dont`,
+	}).then(cb);
 }
 
 async function post(endpoint, body, json = false) {
-    return network(endpoint, 'POST', body, null, json);
+	return network(endpoint, 'POST', body, null, json);
 }
 
 async function patch(endpoint, body) {
-    return network(endpoint, 'PATCH', body, null, true);
+	return network(endpoint, 'PATCH', body, null, true);
 }
 
 async function del(endpoint, body) {
-    return network(endpoint, 'DELETE', body, null, true);
+	return network(endpoint, 'DELETE', body, null, true);
 }
 
 function get(endpoint) {
-    return network(endpoint, 'GET');
+	return network(endpoint, 'GET');
 }
 
 async function network(endpoint, method, body, additionalHeaders, jsonContent) {
-    const headers = {
-        Accept: 'application/json',
-        'auth-token': getCookie('auth-token'), //TODO: replace this with the actual
-        ...additionalHeaders
-    }
-    if (jsonContent)
-        headers['Content-Type'] = 'application/json';
-    const response = await fetch(API_URL + endpoint, {
-        method,
-        headers,
-        body: body || null,
-    });
-    const json = await response.json();
-    const success = !(json.success == false)
-    const returnObject = {
-        success,
-        error: !success ? json.error.message : '',
-        json,
-    };
-    if (!returnObject.success && returnObject.error.includes('auth-token')) {
-        // deleteCookie('auth-token');
-        // window.location.reload();
-    }
-    return returnObject;
+	const headers = {
+		Accept: 'application/json',
+		'auth-token': getCookie('auth-token'), //TODO: replace this with the actual
+		...additionalHeaders,
+	};
+	if (jsonContent) headers['Content-Type'] = 'application/json';
+	const response = await fetch(API_URL + endpoint, {
+		method,
+		headers,
+		body: body || null,
+	});
+	const json = await response.json();
+	const success = !(json.success == false);
+	const returnObject = {
+		success,
+		error: !success ? json.error.message : '',
+		json,
+	};
+	if (!returnObject.success && returnObject.error.includes('auth-token')) {
+		// deleteCookie('auth-token');
+		// window.location.reload();
+	}
+	return returnObject;
 }
